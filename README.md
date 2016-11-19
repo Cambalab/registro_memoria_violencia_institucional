76a40d1616277b2d79b5801e93fd47e28563e731
Jonathan Katz <jona@camba.coop>
2016-11-19 14:12:43 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # registro_memoria_violencia_institucional
 Registro de historias sobre violencia institucional
+
+Para descargar
+
+```
+git clone https://github.com/Cambalab/registro_memoria_violencia_institucional.git
+```
+
+Basado en Angular seed
+
+Para instalar:
+
+```
+cd frontend
+npm install
+npm start
+```
+
+## Plantillas a modificar
+
+```
+app/
+  app.css
+  casos/
+    casos.html
+```
---
+++ frontend/app/casos/casos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('myApp.casos', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/casos', {
+    templateUrl: 'casos/casos.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('data/data.json')
+    .then(function(res){
+      $scope.datos = res.data;
+        });
+}]);
